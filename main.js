8666e6983d521279915c11db3d1bdfcf429d6f26
YasmimSophia <rodrigues.santos.yasmim@escola.pr.gov.br>
2026-05-18 11:01:14 +0000

    aula 4

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
-const abas  = document.querySelectorAll (".aba-conteudo");
+const textos = document.querySelectorAll(".aba-conteudo");
 
-for (let i = 0; i < botoes.length; i++) {
-  botoes[i].onclick = function () {
-    for (let j = 0; j < botoes.length; j++) {
-      botoes[j].classList.remove("ativo");
-      abas[j].classList.remove("ativo");
-    }
-
-    botoes[i].classList.add("ativo");
-  };
-}
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
     for (let j = 0; j < botoes.length; j++) {
